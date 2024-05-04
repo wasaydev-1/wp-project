3e0578d56f3039c4bd28ec08d3d9018047ed0b5a
Abdul Wasay <aw933244@gmail.com>
2024-05-04 21:23:39 +0500

    first commit

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            In the dynamic landscape of today's job market, finding employment
+            entails a multifaceted approach that requires adaptability,
+            resilience, and strategic thinking. Job seekers must navigate
+            through a myriad of opportunities, utilizing various resources and
+            networking channels to secure their desired position.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
